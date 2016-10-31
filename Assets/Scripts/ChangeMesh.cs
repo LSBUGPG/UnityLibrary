176f898eace2f37d4b474a9baedb44ec4e7cfce5
leobyt <leobyt@hotmail.it>
2016-10-31 18:13:29 +0000

    transforming an object to a different one
    It's a script for changing shape when pressing space

---
+++ Assets/Scripts/ChangeMesh.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ChangeMesh : MonoBehaviour
+{
+	public Mesh firstMesh, secondMesh;
+	List<Mesh> meshes;
+	int currentMesh = 0;
+
+	void Awake()
+	{
+		meshes = new List<Mesh>();
+		meshes.Add(firstMesh);
+		meshes.Add(secondMesh);
+	}
+
+	void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.Space))
+		{
+			currentMesh++;
+
+			if (currentMesh == meshes.Count)
+			{
+				currentMesh = 0;
+			}
+
+			gameObject.GetComponent<MeshFilter>().mesh = meshes[currentMesh];
+		}
+	}
+}
