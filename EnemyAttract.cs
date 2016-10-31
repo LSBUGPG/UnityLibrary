156e56b50d60d139dd65c6dca618e1e0c68032a3
notleeandrew <leeandrew1296@hotmail.com>
2016-10-31 14:38:44 +0000

    Add files via upload

---
+++ EnemyAttract.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyAttract : MonoBehaviour {
+
+	public Transform Target;
+	public Transform MyTransform;
+	public float speed = 2f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	void Update(){
+		
+
+	}
+	void OnTriggerStay (Collider theCollision) {
+		if (theCollision.gameObject.tag == "Player") {
+			transform.LookAt (Target);
+			transform.Translate (Vector3.forward * speed * Time.deltaTime);
+		}	
+
+}
+}
