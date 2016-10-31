768d2fbad42eb4c2c735167e0dd6b4a0ad99a2f1
ksosa <brixonic@gmail.com>
2016-10-31 21:06:50 +0000

    Add healhbar support

---
+++ Assets/Scripts/Events/OnHit.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OnHit : MonoBehaviour
+{
+	public int hitDamage;
+
+	void Start()
+	{
+
+	}
+
+	void Update()
+	{
+		if (PlayerStats.isDead)
+		{
+			Debug.Log("Player is dead");
+		}
+
+		else
+		{
+			Debug.Log("Player health: " + PlayerStats.health);
+		}
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.tag == "Trap")
+		{
+			PlayerStats.health -= hitDamage;
+
+			if (PlayerStats.health <= 0)
+			{
+				PlayerStats.health = 0;
+				PlayerStats.isDead = true;
+			}
+		}
+	}
+}
