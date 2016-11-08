9934d6bea38d165af294de927a8d86167e01876e
Paul Sinnett <paul.sinnett@gmail.com>
2016-11-08 19:12:12 +0000

    fix diagonal dodge

--- Assets/Scripts/DodgeMover.cs
+++ Assets/Scripts/DodgeMover.cs
@@ -13,11 +13,11 @@ public class DodgeMover : MonoBehaviour {
 	public bool movementLock = false;
 	public Rigidbody rb;
 	Vector3 latestMoveDir;
-	private int dodgeCount;
 
 
 	// Use this for initialization
 	void Start () {
+		
 		rb = GetComponent<Rigidbody> ();
 		dodgeTimer = dodgeTime;
 		speed = normalSpeed;
@@ -26,83 +26,66 @@ public class DodgeMover : MonoBehaviour {
 	// Update is called once per frame
 	void Update () 
 	{
-		if (Input.GetKey (KeyCode.LeftArrow) && movementLock == false) 
-		{
-			//rb.AddForce(-transform.right * speed);
-			//latestMoveDir.x = -1;
-			transform.Translate (Vector3.left * Time.deltaTime * speed);
-			latestMoveDir = Vector3.back + Vector3.left;
+		if (!movementLock) {
+		
+			latestMoveDir = Vector3.zero;
+
+			if (Input.GetKey (KeyCode.LeftArrow)) {
+
+				latestMoveDir += Vector3.left;
+			}
+
+			if (Input.GetKey (KeyCode.RightArrow)) {
+
+				latestMoveDir += Vector3.right;
+			}
+
+			if (Input.GetKey (KeyCode.UpArrow)) {
+
+				latestMoveDir += Vector3.forward;
+			}
+
+			if (Input.GetKey (KeyCode.DownArrow)) {
+
+				latestMoveDir += Vector3.back;
+			}
 		}
 
-		if (Input.GetKey (KeyCode.RightArrow) && movementLock == false) {
-
-			//rb.AddForce (transform.right * speed);
-			//latestMoveDir.x = 1;
-			transform.Translate (Vector3.right * Time.deltaTime * speed);
-
-			latestMoveDir = Vector3.forward + Vector3.right;
-		}
-
-		if (Input.GetKey (KeyCode.UpArrow) && movementLock == false) {
-
-			//rb.AddForce (transform.forward * speed);
-			//latestMoveDir.z = 1;
-			transform.Translate (Vector3.forward * Time.deltaTime * speed);
-			latestMoveDir = Vector3.forward + Vector3.left;
-		}
-
-		if (Input.GetKey (KeyCode.DownArrow) && movementLock == false) {
-
-			//rb.AddForce (-transform.forward * speed);
-			//latestMoveDir.z = -1;
-			transform.Translate (Vector3.back * Time.deltaTime * speed);
-			latestMoveDir = Vector3.back + Vector3.right;
-		}
-
-
-
-		if ((Input.GetKeyUp(KeyCode.UpArrow) && movementLock == false) || (Input.GetKeyUp(KeyCode.DownArrow) && movementLock == false)) {
-			latestMoveDir.z = 0;
-		}
-		if ((Input.GetKeyUp(KeyCode.LeftArrow)  && movementLock == false) || (Input.GetKeyUp(KeyCode.RightArrow) && movementLock == false)) {
-			latestMoveDir.x = 0;
-		}
-
-
-		if (Input.GetKeyDown (KeyCode.Space) && dodgeLock == false && latestMoveDir != Vector3.zero) {
+		if (Input.GetMouseButtonDown (0) && dodgeLock == false && latestMoveDir != Vector3.zero) {
+			
 			speed = dodgeSpeed;
 			movementLock = true;
 			timerLock = true;
 		}
 
 		if (timerLock == true) {
+			
 			dodgeTimer = dodgeTimer - 1;
 			dodgeLock = true;
 		}
-		if (movementLock == true) {
-			//rb. = latestMoveDir * speed * Time.deltaTime;
-			//transform.Translate (latestMoveDir * Time.deltaTime * speed);
-			rb.MovePosition(transform.position + latestMoveDir * speed * Time.deltaTime);
-			//rb.AddForce (latestMoveDir * speed);
-		}
+
+		rb.MovePosition(transform.position + latestMoveDir * speed * Time.deltaTime);
+
 		if (dodgeTimer <= 0) {
+			
 			speed = 0;
 			movementLock = false;
 
 		}
 		if (dodgeTimer <= -30) {
+			
 			timerLock = false;
 			dodgeTimer = dodgeTime;
 			speed = normalSpeed;
 		}
 			
 		if (dodgeTimer == dodgeTime) {
+			
 			dodgeLock = false;
 			timerLock = false;
 		}
 
 		//Nomral move
-		//rb.velocity = Vector3.ClampMagnitude (rb.velocity, speed);
 		rb.velocity = Vector3.zero;
 	}
 }
