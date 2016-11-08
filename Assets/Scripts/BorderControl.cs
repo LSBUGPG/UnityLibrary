cbd7e22cfe33e14c1c6bb38240d88ebae2b8abc2
Murray <silent.hero66@hotmail.co.uk>
2016-11-08 16:46:30 +0000

    My 3D Level Design project
    The dodge component is not working properly. It will not move
    diagonally. Whenever I attempt it, it only moves directly up or down and
    does not recognise the simultainious inputs.

---
+++ Assets/Scripts/BorderControl.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BorderControl : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	// Update is called once per frame
+	void OnTriggerEnter (Collider other) {
+		print ("Hitmonlee");
+		if (other.gameObject.tag == "Projectile") {
+			Destroy (other.gameObject);
+		}
+	}
+}
