997dfd7b7a3aea09bac8c7dafa31ea99a994f0fd
Jamie Hagen <jpdhagen@aol.com>
2016-11-29 15:59:15 +0000

    Random Projectiles
    Random spawns objects within determined limits.

---
+++ RandomProjectiles.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RandomProjectiles : MonoBehaviour {
+
+	public float timer = 0f;
+	public float timePassing = 1f;
+	public GameObject prefab;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		timer += timePassing;
+		if (timer == 30f) {
+			timer = 0;
+			GameObject obj = (GameObject)Instantiate(prefab,new Vector3(Random.Range(1,20),Random.Range(1,20),Random.Range(1,20)),Quaternion.identity);
+		}
+	
+	}
+}
