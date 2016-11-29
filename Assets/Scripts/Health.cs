a8b19f9bf7f1fff5476cb5f3d25bdc10c3cb2ed5
Murray <silent.hero66@hotmail.co.uk>
2016-11-29 16:04:45 +0000

    Health system
    The health bar is depleated at the player is hit with projetiles.

---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Health : MonoBehaviour {
+
+	public int lives = 10;
+	public Slider healthSLider;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void OnTriggerEnter (Collider other)
+	{
+		if (other.tag == "Projectile") {
+			lives--;
+			Destroy (other.gameObject);
+			healthSLider.value = lives;
+
+			if (lives <= 0) {
+				GameOver ();
+			}
+		}
+	}
+
+	void GameOver ()
+	{
+		Destroy (gameObject);
+	}
+}
