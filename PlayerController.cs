aad4429c5639f7362a55e4d68d66dc1452d23906
Joshua Child <joshua.child@hotmail.co.uk>
2016-12-13 14:25:35 +0000

    Player Controller
    Controls player character, keeps track of pick ups collected and changes scene after X amount of pick ups have been collected by the player.

---
+++ PlayerController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerController : MonoBehaviour {
+
+	public string levelToLoad;
+	public float speed;
+	public Vector3 PlayerStart;
+	public GameObject Player;
+	private Rigidbody rb;
+	public int PickUpsCollect;
+	private bool PlayerWin = false;
+
+	// Use this for initialization
+	void Start ()
+	{
+		rb = GetComponent<Rigidbody> ();
+		PlayerStart = Player.transform.position;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate ()
+	{
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0f, moveVertical);
+
+		rb.AddForce (movement * speed);
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject.CompareTag ("Pick Up")) {
+			other.gameObject.SetActive (false);
+			PickUpsCollect += 1;
+		}
+
+		if (PickUpsCollect >= 7)
+			PlayerWin = true;
+
+		if(PlayerWin == true)
+			SceneManager.LoadScene(levelToLoad);
+	}
+}
