efbe23b9813b1e23dc9f0edad26d0cfff88ac30d
ClockworkAlexander <KarlAlexander9@Outlook.com>
2016-12-13 18:28:18 +0000

    SCRIPT_Rocket
    The code for the Rocket prefab, controlling its speed

---
+++ Rocket.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rocket : MonoBehaviour {
+
+	public float speed = 20.0f;
+	public float life = 5.0f;
+	public float damageDealt;
+
+	// Use this for initialization
+	void Start () {
+	
+		Invoke ("Kill", life);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		transform.position += transform.forward * speed * Time.deltaTime;
+	}
+
+	//Destory gameobject when Kill is called
+	void Kill()
+	{
+		Destroy (gameObject);
+	}
+}
