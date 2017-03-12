025040215a7140eaa3798f023b8a4b3c44fa0d33
Joshua Taylor <Tayloj36@gmail.com>
2017-03-12 17:39:30 +0000

    MeleeattackImproved.
    Readme:
    Meleeattack Improved.
    
    Basicly I added a value that can be customised from within the script that can be modified to define
    any new weapons that the player might create. I also created some sample weapons that use this system.
    
    The Enemy preset has various values for dammage. Each of the weapons are defined by tags, the
    coder can use theese scripts to create more weapons.
    
    The weapons start out disabled but are enabled when the mouse is pressed, the weapon appears and 
    does dammage before promtly dissapearing. the weapon is defined by what button has been pressed
    prior to attacking.
    
    Yet again, I can only upload the scripts thanks to the file limits.

---
+++ AttackAnim.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackAnim : MonoBehaviour {
+
+	public GameObject TestStick;
+	public GameObject weaponplaceholder1;
+	public GameObject weaponplaceholder2;
+	public int currentitem;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Alpha1)) {
+			currentitem = 0;
+			}
+		if (Input.GetKeyDown (KeyCode.Alpha2)) {
+			currentitem = 1;
+		}
+		if (Input.GetKeyDown (KeyCode.Alpha3)) {
+			currentitem = 2;
+		}
+
+		if (Input.GetKeyDown (KeyCode.Mouse0)) {
+			if (currentitem == 0) {
+				TestStick.gameObject.SetActive (true);
+				Invoke ("Clearweapon", 1);
+			}
+			if (Input.GetKeyDown (KeyCode.Mouse0)) {
+				if (currentitem == 1) {
+					weaponplaceholder1.SetActive (true);
+					Invoke ("Clearweapon", 1);
+				}
+				if (Input.GetKeyDown (KeyCode.Mouse0)) {
+					if (currentitem == 2) {
+						weaponplaceholder2.SetActive (true);
+						Invoke ("Clearweapon", 1);
+					}
+
+				}
+					
+			}
+		}
+	}
+	void Clearweapon () {
+	TestStick.gameObject.SetActive (false);
+	weaponplaceholder1.SetActive (false);
+	weaponplaceholder2.SetActive (false);
+	}
+}
